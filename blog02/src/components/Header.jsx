200bb0a72c5e6a831430bbf1cac4efe09d709eb3
zsecq94 <zsecq94@gmail.com>
2024-02-26 15:46:40 +0900

    feat : message-box components update

--- blog02/src/components/Header.jsx
+++ blog02/src/components/Header.jsx
@@ -1,19 +1,43 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
-import Lottie from "react-lottie";
-import lottie from "@/assets/json/lottie/header-animate";
+import lottieJson from "@/assets/json/lottie/header-animate";
+import Lottie from "react-lottie-player";
+import { useEffect, useState } from "react";
 
 const Header = () => {
   const location = useLocation();
-  const lottieOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: lottie,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
+  const navigate = useNavigate();
+
+  const [searchVal, setSearchVal] = useState("");
+  const [searchBox, setSearchBox] = useState(false);
+
+  const handleSearchBox = () => {
+    setSearchBox(!searchBox);
   };
+
+  const onSearchBtn = (val) => {
+    if (val.length > 1) {
+      setSearchVal("");
+      setSearchBox(false);
+      navigate(`/search/${val}`);
+    } else {
+      alert("검색어를 2자 이상 입력해주세요.");
+    }
+  };
+
+  const clickOutside = (event) => {
+    if (!event.target.closest(".search-box") && !event.target.closest(".ico_search")) {
+      setSearchBox(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("click", clickOutside);
+    return () => {
+      window.removeEventListener("click", clickOutside);
+    };
+  }, []);
 
   return (
     <header>
@@ -23,7 +47,7 @@ const Header = () => {
             <strong>DEV</strong>.LOG
           </Link>
           <div className="lottie-box">
-            <Lottie options={lottieOptions} height={35} width={35} />
+            <Lottie loop animationData={lottieJson} play style={{ width: 60, height: 60 }} />
           </div>
         </div>
         <ul className="gnb-box">
@@ -36,14 +60,38 @@ const Header = () => {
           <Link className={location.pathname === "/algo" ? "active" : ""} to={"/algo"}>
             알고리즘
           </Link>
-          {/* <Link className={location.pathname === "/component" ? "active" : ""} to={"/component"}>
+          <Link className={location.pathname === "/component" ? "active" : ""} to={"/component"}>
             컴포넌트
-          </Link> */}
+          </Link>
           <Link className={location.pathname === "/port" ? "active" : ""} to={"/port"}>
             포트폴리오
           </Link>
-          <img src="/src/assets/images/icons/ico_search.png" alt="" />
+          <img className="ico_search" onClick={handleSearchBox} src="/src/assets/images/icons/ico_search.png" alt="" />
         </ul>
+      </div>
+      <div className={searchBox ? "search-box active" : "search-box"}>
+        <div className="category-box">
+          <span onClick={() => onSearchBtn("React")}>#React</span>
+          <span onClick={() => onSearchBtn("Vue")}>#Vue</span>
+          <span onClick={() => onSearchBtn("Nods.js")}>#Nods.js</span>
+          <span onClick={() => onSearchBtn("백준")}>#백준</span>
+          <span onClick={() => onSearchBtn("프로그래머스")}>#프로그래머스</span>
+          <span onClick={() => onSearchBtn("버튼")}>#버튼</span>
+          <span onClick={() => onSearchBtn("알고리즘")}>#알고리즘</span>
+        </div>
+        <div className="inp-box">
+          <input
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                onSearchBtn(searchVal);
+              }
+            }}
+            onChange={(e) => setSearchVal(e.target.value)}
+            type="text"
+            placeholder="검색어를 입력해주세요."
+          />
+          <button onClick={() => onSearchBtn(searchVal)}>검색</button>
+        </div>
       </div>
     </header>
   );
