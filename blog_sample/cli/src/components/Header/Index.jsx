28b0e63359a1c437ac8781ebb4bafe0dc90e3940
zsecq94 <zsecq94@gmail.com>
2024-03-06 00:18:06 +0900

    feat : update

--- blog_sample/cli/src/components/Header/Index.jsx
+++ blog_sample/cli/src/components/Header/Index.jsx
@@ -1,10 +1,25 @@
+import { Link } from "react-router-dom";
 import "./Index.scss";
 
 const Header = () => {
   return (
     <header>
-      <div className="con">hi</div>
-      <div className="con">hi</div>
+      <article>
+        <section>
+          <Link to={"/"} className="logo">
+            <b>DEV.LOG</b>
+          </Link>
+
+          <ul className="gnb">
+            <Link>개발</Link>
+            <Link>포트폴리오</Link>
+          </ul>
+        </section>
+
+        <section>
+          <input type="text" />
+        </section>
+      </article>
     </header>
   );
 };
