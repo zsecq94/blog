8c81988ecc192fda2abf1bc110f81e6a8757e8e6
zsecq94 <zsecq94@gmail.com>
2024-03-16 18:35:40 +0900

    feat : update

--- blog_sample_2/client/src/components/Header.jsx
+++ blog_sample_2/client/src/components/Header.jsx
@@ -1,17 +1,24 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <header>
-      <h2>
-        <b>DEV</b>.LOG
-      </h2>
-      <ul className="gnb">
-        <li>TECH</li>
-        <li>ALGORITHM</li>
-        <li>PORTFOLIO</li>
-        <li>Tech</li>
-        <li>Algorithm</li>
-        <li>Portfolio</li>
-      </ul>
+      <article>
+        <p>
+          <b>DEV</b>.LOG
+        </p>
+        <ul className="gnb">
+          <li>
+            <Link to={"/tech"}>TECH</Link>
+          </li>
+          <li>
+            <Link to={"/algo"}>ALGOLITHM</Link>
+          </li>
+          <li>
+            <Link to={"/port"}>PORTFOLIO</Link>
+          </li>
+        </ul>
+      </article>
     </header>
   );
 };
