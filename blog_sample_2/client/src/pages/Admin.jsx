225ba17b3767e1ba385b18b5f2eb24a25538cf6f
zsecq94 <zsecq94@gmail.com>
2024-03-23 16:32:03 +0900

    faet : admin page update

--- blog_sample_2/client/src/pages/Admin.jsx
+++ blog_sample_2/client/src/pages/Admin.jsx
@@ -1,7 +1,66 @@
+import { useEffect, useState } from "react";
+
 const Admin = () => {
+  const [isLogin, setIsLogin] = useState(false);
+  const [userInfo, setUserInfo] = useState({
+    id: null,
+    pw: null,
+  });
+
+  const onChangeId = (e) => {
+    setUserInfo((prevState) => ({
+      ...prevState,
+      id: e.target.value,
+    }));
+  };
+
+  const onChangePw = (e) => {
+    setUserInfo((prevState) => ({
+      ...prevState,
+      pw: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(userInfo);
+    setIsLogin(true);
+  };
+
   return (
     <article>
-      <h2>Admin</h2>
+      {isLogin ? (
+        <section>로그인</section>
+      ) : (
+        <section className="modal">
+          <div className="modal-box">
+            <form onSubmit={onSubmit}>
+              <div className="inp-box">
+                <label htmlFor="id">아이디</label>
+                <input
+                  type="text"
+                  id="id"
+                  placeholder="ID"
+                  onChange={onChangeId}
+                  value={userInfo.id}
+                />
+              </div>
+
+              <div className="inp-box">
+                <label htmlFor="pw">비밀번호</label>
+                <input
+                  type="password"
+                  id="pw"
+                  placeholder="PW"
+                  onChange={onChangePw}
+                  value={userInfo.pw}
+                />
+              </div>
+              <button type="submit">로그인</button>
+            </form>
+          </div>
+        </section>
+      )}
     </article>
   );
 };
